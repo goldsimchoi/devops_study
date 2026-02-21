357905a2c521afc3441390e52ad943ca4a455d35
Bm0p <gdool88@gmail.com>
2026-02-22 04:48:02 +0900

    사진 추가 및 변경

--- apps/frontend/blog_bm0p/src/config.ts
+++ apps/frontend/blog_bm0p/src/config.ts
@@ -8,8 +8,8 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Fuwari",
-	subtitle: "Demo Site",
+	title: "Bm0p",
+	subtitle: "blog",
 	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -53,9 +53,9 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	avatar: "assets/images/yukino.webp", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	name: "Bm0p",
+	bio: "Noob Hacker!",
 	links: [
 		{
 			name: "Twitter",
